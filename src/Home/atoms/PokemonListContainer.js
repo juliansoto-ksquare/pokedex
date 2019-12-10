ae30b151571473b2792ee9238a2cdac81d04f6c7
juliansoto <juliansotoponce@gmail.com>
2019-12-10 11:06:31 -0600

    add media queries to pokemons list

--- src/Home/atoms/PokemonListContainer.js
+++ src/Home/atoms/PokemonListContainer.js
@@ -2,10 +2,22 @@ import styled from 'styled-components';
 
 const PokemonListContainer = styled.ul`
     display: grid;
-    grid-template-columns: 2fr 2fr;
+    grid-template-columns: 1fr;
     grid-gap: 1em;
     list-style: none;
     padding: 0;
+
+    @media (min-width: 360px) {
+        grid-template-columns: 1fr 1fr;
+    }
+
+    @media (min-width: 720px) {
+        grid-template-columns: 1fr 1fr 1fr;
+    }
+
+    @media (min-width: 1023px) {
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+    }
 `;
 
 export default PokemonListContainer;
